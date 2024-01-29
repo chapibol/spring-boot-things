4ecbbd5b6c067c127a83f8f02b57df8d30164f35
Luis Velasco <luis.velasco.c@outlook.com>
2024-01-29 16:15:49 -0500

    refactor some variable names in CustomerSvc, add unit tests for remaining methods

--- src/test/java/com/amigoscode/customer/CustomerSvcTest.java
+++ src/test/java/com/amigoscode/customer/CustomerSvcTest.java
@@ -1,19 +1,21 @@
 package com.amigoscode.customer;
 
+import com.amigoscode.exception.DuplicateResourceException;
+import com.amigoscode.exception.RequestValidationException;
 import com.amigoscode.exception.ResourceNotFoundException;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CustomerSvcTest {
@@ -66,26 +68,172 @@ class CustomerSvcTest {
         CustomerRegistrationRequest request = new CustomerRegistrationRequest(FAKER.name().fullName(), email, 22);
         when(customerDao.existsPersonWithEmail(email)).thenReturn(false);
         svcToTest.addCustomer(request);
-        verify(customerDao).insertCustomer(new Customer(request.name(), request.email(), request.age()));
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(customerDao).insertCustomer(customerArgumentCaptor.capture());
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+        assertThat(capturedCustomer.getId()).isNull();
+        assertThat(capturedCustomer.getName()).isEqualTo(request.name());
+        assertThat(capturedCustomer.getAge()).isEqualTo(request.age());
+        assertThat(capturedCustomer.getEmail()).isEqualTo(request.email());
+    }
+
+    @Test
+    void addCustomerEmailExistsError() {
+        String email = FAKER.internet().safeEmailAddress();
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(FAKER.name().fullName(), email, 22);
+        when(customerDao.existsPersonWithEmail(email)).thenReturn(true);
+
+        assertThatThrownBy(() -> svcToTest.addCustomer(request)).isInstanceOf(DuplicateResourceException.class)
+                .hasMessage("email already taken");
+
+        // test that the line that should never execute actually does not ever execute
+        verify(customerDao, never()).insertCustomer(any());
     }
 
     @Test
     void deleteCustomer() {
+        Long id = 1L;
+        when(customerDao.existsCustomerById(id)).thenReturn(true);
+        svcToTest.deleteCustomer(id);
+        verify(customerDao).existsCustomerById(id);
+        verify(customerDao).deleteCustomerById(id);
     }
 
     @Test
-    void updateCustomer() {
+    void deleteCustomerThrowsError() {
+        Long id = 1L;
+        when(customerDao.existsCustomerById(id)).thenReturn(false);
+        assertThatThrownBy(() -> svcToTest.deleteCustomer(id)).isInstanceOf(ResourceNotFoundException.class)
+                .hasMessage("customer with id %s could not be found.".formatted(id));
+        verify(customerDao).existsCustomerById(id);
+        verify(customerDao, never()).deleteCustomerById(anyLong());
     }
 
     @Test
-    void testUpdateCustomer() {
+    void updateCustomerNameChanged() {
+        Long customerId = 1L;
+        Customer ogCustomer = new Customer(customerId,"Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.of("Mateo Messi"), Optional.of("lio@messi.com"), Optional.of(36));
+
+        when(customerDao.getCustomerById(customerId)).thenReturn(Optional.of(ogCustomer));
+        svcToTest.updateCustomer(customerId, updateRequest);
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(customerDao).updateCustomer(customerArgumentCaptor.capture());
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+        assertThat(capturedCustomer.getId()).isEqualTo(customerId);
+        assertThat(capturedCustomer.getName()).isEqualTo(updateRequest.name().get());
+        assertThat(capturedCustomer.getEmail()).isEqualTo(updateRequest.email().get());
+        assertThat(capturedCustomer.getAge()).isEqualTo(updateRequest.age().get());
     }
 
     @Test
-    void isCustomerDataValid() {
+    void updateCustomerAgeChanged() {
+        Long customerId = 1L;
+        Customer ogCustomer = new Customer(customerId,"Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.of("Lio Messi"), Optional.of("lio@messi.com"), Optional.of(26));
+
+        when(customerDao.getCustomerById(customerId)).thenReturn(Optional.of(ogCustomer));
+        svcToTest.updateCustomer(customerId, updateRequest);
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(customerDao).updateCustomer(customerArgumentCaptor.capture());
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+        assertThat(capturedCustomer.getId()).isEqualTo(customerId);
+        assertThat(capturedCustomer.getName()).isEqualTo(updateRequest.name().get());
+        assertThat(capturedCustomer.getEmail()).isEqualTo(updateRequest.email().get());
+        assertThat(capturedCustomer.getAge()).isEqualTo(updateRequest.age().get());
+    }
+
+    @Test
+    void updateCustomerEmailChanged() {
+        Long customerId = 1L;
+        Customer ogCustomer = new Customer(customerId,"Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.of("Lio Messi"), Optional.of("lio10@messi.com"), Optional.of(36));
+
+        when(customerDao.getCustomerById(customerId)).thenReturn(Optional.of(ogCustomer));
+        svcToTest.updateCustomer(customerId, updateRequest);
+        ArgumentCaptor<Customer> customerArgumentCaptor = ArgumentCaptor.forClass(Customer.class);
+        verify(customerDao).updateCustomer(customerArgumentCaptor.capture());
+        Customer capturedCustomer = customerArgumentCaptor.getValue();
+        assertThat(capturedCustomer.getId()).isEqualTo(customerId);
+        assertThat(capturedCustomer.getName()).isEqualTo(updateRequest.name().get());
+        assertThat(capturedCustomer.getEmail()).isEqualTo(updateRequest.email().get());
+        assertThat(capturedCustomer.getAge()).isEqualTo(updateRequest.age().get());
+    }
+
+    @Test
+    void updateCustomerNoChangesDetected() {
+        Long customerId = 1L;
+        Customer ogCustomer = new Customer(customerId,"Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.empty(), Optional.empty(), Optional.empty());
+
+        when(customerDao.getCustomerById(customerId)).thenReturn(Optional.of(ogCustomer));
+        assertThatThrownBy(() -> svcToTest.updateCustomer(customerId, updateRequest)).isInstanceOf(RequestValidationException.class)
+                .hasMessage("No data changes found");
+        verify(customerDao, never()).updateCustomer(any());
+    }
+
+    @Test
+    void updateCustomerNoChangesDetectedSameData() {
+        Long customerId = 1L;
+        Customer ogCustomer = new Customer(customerId,"Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.of("Lio Messi"), Optional.of("lio@messi.com"), Optional.of(36));
+
+        when(customerDao.getCustomerById(customerId)).thenReturn(Optional.of(ogCustomer));
+        assertThatThrownBy(() -> svcToTest.updateCustomer(customerId, updateRequest)).isInstanceOf(RequestValidationException.class)
+                .hasMessage("No data changes found");
+        verify(customerDao, never()).updateCustomer(any());
+    }
+
+    @Test
+    void isCustomerDataValidNoChangedData() {
+        Customer ogCustomer = new Customer("Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.of("Lio Messi"), Optional.of("lio@messi.com"), Optional.of(36));
+
+        assertThat(svcToTest.isCustomerDataValid(ogCustomer, updateRequest)).isFalse();
+    }
+
+    @Test
+    void isCustomerDataValidNoChangedDataEmptyRequest() {
+        Customer ogCustomer = new Customer("Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.empty(), Optional.empty(), Optional.empty());
+
+        assertThat(svcToTest.isCustomerDataValid(ogCustomer, updateRequest)).isFalse();
+    }
+
+    @Test
+    void isCustomerDataValidNameChanged() {
+        Customer ogCustomer = new Customer("Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.of("Mateo Messi"), Optional.of("lio@messi.com"), Optional.of(36));
+
+        assertThat(svcToTest.isCustomerDataValid(ogCustomer, updateRequest)).isTrue();
+    }
+
+    @Test
+    void isCustomerDataValidEmailChanged() {
+        Customer ogCustomer = new Customer("Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.of("Lio Messi"), Optional.of("lio10@messi.com"), Optional.of(36));
+
+        assertThat(svcToTest.isCustomerDataValid(ogCustomer, updateRequest)).isTrue();
+    }
+
+    @Test
+    void isCustomerDataValidNameAndAgeChanged() {
+        Customer ogCustomer = new Customer("Lio Messi", "lio@messi.com", 36);
+        CustomerUpdateRequest updateRequest = new CustomerUpdateRequest(Optional.of("Mateo Messi"), Optional.of("lio@messi.com"), Optional.of(15));
+
+        assertThat(svcToTest.isCustomerDataValid(ogCustomer, updateRequest)).isTrue();
     }
 
     @Test
     void toCustomer() {
+        CustomerRegistrationRequest customerRegistrationRequest = new CustomerRegistrationRequest(
+                FAKER.name().fullName(), FAKER.internet().safeEmailAddress(), 22);
+
+        Customer mappedCustomer = svcToTest.toCustomer(customerRegistrationRequest);
+
+        assertThat(mappedCustomer.getId()).isNull();
+        assertThat(mappedCustomer.getName()).isEqualTo(customerRegistrationRequest.name());
+        assertThat(mappedCustomer.getEmail()).isEqualTo(customerRegistrationRequest.email());
+        assertThat(mappedCustomer.getAge()).isEqualTo(customerRegistrationRequest.age());
     }
 }
