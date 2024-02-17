5d8db3c3e1925b0a709a9bb165ea230acc3c3abf
Luis Velasco <luis.velasco.c@outlook.com>
2024-02-16 22:38:16 -0500

    Update spring boot to 3.2.2 and remove comannd line runner from main class

--- src/main/java/com/amigoscode/Main.java
+++ src/main/java/com/amigoscode/Main.java
@@ -1,14 +1,7 @@
 package com.amigoscode;
 
-import com.amigoscode.customer.Customer;
-import com.amigoscode.customer.CustomerRepository;
-import com.github.javafaker.Faker;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.List;
 
 
 @SpringBootApplication
@@ -17,13 +10,4 @@ public class Main {
        SpringApplication.run(Main.class, args);
     }
 
-    @Bean
-    public CommandLineRunner runner(CustomerRepository customerRepository) {
-        return args -> {
-            Faker faker = new Faker();
-            Customer customerOne = new Customer(faker.name().firstName(), faker.name().lastName() + "@gmail.com", faker.number().numberBetween(18, 45));
-            Customer customerTwo = new Customer(faker.name().firstName(), faker.name().lastName() + "@gmail.com", faker.number().numberBetween(18, 45));
-            customerRepository.saveAll(List.of(customerOne, customerTwo));
-        };
-    }
 }
