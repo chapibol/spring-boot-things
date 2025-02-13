c4987fb07ecdbbc9debcfcdb12563d856765f757
Luis Velasco <luis.velasco.c@outlook.com>
2025-02-12 19:14:33 -0500

    Optimize import statements

--- src/test/java/com/amigoscode/customer/CustomerSvcTest.java
+++ src/test/java/com/amigoscode/customer/CustomerSvcTest.java
@@ -11,9 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.*;
 
